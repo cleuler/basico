61aee111b51e59b0c304612fd0e131744d1eed56
cleuler <cleuler@gmail.com>
2020-02-20 18:02:54 -0300

    Update vars.php

--- vars.php
+++ vars.php
@@ -1,1 +1,7 @@
 <?php
+$nome = "João";
+$sobrenome = 'da Silva';
+echo "$sobrenome, $nome"; // com áspas duplas mesclam-se os conteúdos das variáveis com a vírgula e o espaço
+echo '<br>';              // para pular para próxima linha da saída
+echo '$sobrenome, $nome'; // com apóstrofe NÃO se mesclam os conteúdos das variáveis com a vírgula e o espaço
+
