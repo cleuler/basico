aad1e56326f66b0fe2745639af7dad6a86024239
cleuler <cleuler@gmail.com>
2020-02-23 18:54:23 -0300

    Update cte.php

--- cte.php
+++ cte.php
@@ -1,9 +1,12 @@
 <?php
-define('Pi' , 3.14159265359);
-define('e'  , 2.71828);
+define('Pi' , 3.14159265358979323846);
+define('e'  , 2.7182818284590452354);
 define('Phi', 1.61803399);
 
+include_once 'newline.php'; // carrega o arquivo com a função new line: nl(n = num.linhas)
+print 'Pi/Phi = ';
 print_r(Pi / Phi);
-
+nl(1);
 print "Conferir lista das constantes do PHP: ";
-echo 'https://www.php.net/manual/pt_BR/math.constants.php';
+echo '<a href="https://www.php.net/manual/pt_BR/math.constants.php">math.constants.php</a>';
+nl(1);
