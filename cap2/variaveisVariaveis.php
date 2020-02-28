181ad17dcd01e6ad2b6f58ba727afa3ee5423545
cleuler <cleuler@gmail.com>
2020-02-28 11:00:44 -0300

    update variaveisVariaveis.php p. 29 e p. 96.

--- cap2/variaveisVariaveis.php
+++ cap2/variaveisVariaveis.php
@@ -15,3 +15,11 @@ foreach ($produto as $chave => $value) {
     $objeto->$chave = $value;
 }
 print_r($objeto);
+nl();
+
+// define o nome da variável
+$variavel = 'nome';
+// cria variável identificada pelo conteúdo de $variavel
+$$variavel = 'Maria';
+// exibe o conteúdo da variável $nome na tela
+echo $nome; // resultado = Maria
