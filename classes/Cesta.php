3ed5eee761d44bdb3c33f5e292d9bdfc1839695d
cleuler <cleuler@gmail.com>
2020-03-03 14:04:28 -0300

    update Cesta.php

--- classes/Cesta.php
+++ classes/Cesta.php
@@ -12,5 +12,40 @@ class Cesta
         $this->itens = array();
     }
 
+    public function addItem(Produto $p) // type hinting
+    {
+        $this->itens[] = $p;
+    }
 
+    /**
+     * @return array
+     */
+    public function getItens(): array
+    {
+        return $this->itens;
+    }
+
+    /**
+     * @return false|string
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param array $itens
+     */
+    public function setItens(array $itens): void
+    {
+        $this->itens = $itens;
+    }
+
+    /**
+     * @param false|string $time
+     */
+    public function setTime($time): void
+    {
+        $this->time = $time;
+    }
 }
